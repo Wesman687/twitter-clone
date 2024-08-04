bbc521b9827f939b176c7570ba0bafe600bfc8e4
unknown <wesman687@gmail.com>
2024-08-03 23:28:29 -0400

    delete function added

--- pages/components/Tweet.js
+++ pages/components/Tweet.js
@@ -1,13 +1,14 @@
 import { db } from "@/firebase";
-import { openCommentModal, setCommentTweet } from "@/redux/modalSlice";
+import { openCommentModal, openLoginModal, setCommentTweet } from "@/redux/modalSlice";
 import {
   ChartBarIcon,
   ChatIcon,
   HeartIcon,
+  TrashIcon,
   UploadIcon,
 } from "@heroicons/react/outline";
 import {HeartIcon as FilledHeartIcon} from "@heroicons/react/outline";
-import { arrayRemove, arrayUnion, doc, onSnapshot, updateDoc } from "firebase/firestore";
+import { arrayRemove, arrayUnion, deleteDoc, doc, onSnapshot, updateDoc } from "firebase/firestore";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Moment from "react-moment";
@@ -18,9 +19,14 @@ export default function Tweet({ data, id }) {
   const router = useRouter()
   const user = useSelector(state => state.user)
   const [likes, setLikes] = useState([])
+  const [comments, setComments] = useState([])
 
 async function likeComment(e){
   e.stopPropagation()
+  if (!user.username) {
+    dispatch(openLoginModal())
+    return
+  }
   if (likes.includes(user.uid)){
     await updateDoc(doc(db, "posts", id), {
       likes: arrayRemove(user.uid),
@@ -32,10 +38,15 @@ async function likeComment(e){
   }
   
 }
+async function deleteTweet(e){
+  e.stopPropagation()
+  await deleteDoc(doc(db, "posts", id))
+}
 useEffect(() => {
   if (!id) return
   const unsubscribe = onSnapshot(doc(db, "posts", id), (doc) => {
-    setLikes(doc.data().likes)
+    setLikes(doc.data()?.likes)
+    setComments(doc.data()?.comments)
   })
   return unsubscribe
 }, [])
@@ -53,8 +64,13 @@ useEffect(() => {
       />
       <div className="p-3 ml-16 text-gray-500 flex space-x-14">
         <div
+        className="flex justify-center items-center space-x-2"
           onClick={(e) => {
             e.stopPropagation()
+            if (!user.username) {
+              dispatch(openLoginModal())
+              return
+            }
             dispatch(
               setCommentTweet({
                 id: id,
@@ -68,6 +84,7 @@ useEffect(() => {
           }}
         >
           <ChatIcon className="w-5 cursor-pointer hover:text-green-400" />
+          {comments?.length > 0 && <span>{comments.length}</span>}
         </div>
         <div
         className="flex justify-center items-center space-x-2"
@@ -75,6 +92,11 @@ useEffect(() => {
           {likes.includes(user.uid) ? <FilledHeartIcon className="w-5 text-pink-500 cursor-pointer" /> : <HeartIcon className="w-5 cursor-pointer hover:text-pink-500" />}
        {likes.length > 0 && <span>{likes.length}</span>}
         </div>
+        {user.uid === data?.uid && (
+          <div className="cursor-pointer hover:text-red-600"
+          onClick={deleteTweet}>
+            <TrashIcon className="w-5" />
+          </div>)}
         <ChartBarIcon className="w-5 cursor-not-allowed" />
         <UploadIcon className="w-5 cursor-not-allowed" />
       </div>
--- pages/components/TweetInput.js
+++ pages/components/TweetInput.js
@@ -23,7 +23,7 @@ export default function TweetInput() {
     <div className="flex space-x-3 p-3 border-b border-gray-700">
       <img
         className="w-11 h-11 rounded-full object-cover"
-        src={user.photoUrl}
+        src={user.photoUrl || "/assets/twitter-logo.png"}
       ></img>
       <div className="w-full">
         <textarea
